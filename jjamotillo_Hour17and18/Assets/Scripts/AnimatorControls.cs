39463003131c7ccc8c8a9392cb246e13e57b55e6
vveeoohh <justinjamotillo@gmail.com>
2024-11-24 23:27:41 -0800

    Hour17 and 18
    Animation

---
+++ jjamotillo_Hour17and18/Assets/Scripts/AnimatorControls.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorControls : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetTrigger("ColorChange");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetTrigger("Scale");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetTrigger("Rotation");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("Position");
+        }
+    }
+}
