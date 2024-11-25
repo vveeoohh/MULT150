e066ce56a37c38e259033441e10043c5380a1ef2
vveeoohh <justinjamotillo@gmail.com>
2024-11-24 23:44:34 -0800

    Hour 17 and 18
    Animation

--- jjamotillo_Hour17and18/Assets/Scripts/AnimatorControls.cs
+++ jjamotillo_Hour17and18/Assets/Scripts/AnimatorControls.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AnimatorControls : MonoBehaviour
 {
-    private Animator animator;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
