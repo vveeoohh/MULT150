a80a913b4c26fb5f921cd872cb980e2c81e26316
vveeoohh <justinjamotillo@gmail.com>
2024-12-15 20:00:56 -0800

    Final - Part 1
    Final Project (Amazing Racer)

---
+++ jjamotillo_Final/Assets/Modifications.cs
@@ -0,0 +1,34 @@
+/*                                                                                                                                                                                                                             
+                                                            dddddddd                                                                                                                                                             
+MMMMMMMM               MMMMMMMM                             d::::::d  iiii     ffffffffffffffff    iiii                                                tttt            iiii                                                      
+M:::::::M             M:::::::M                             d::::::d i::::i   f::::::::::::::::f  i::::i                                            ttt:::t           i::::i                                                     
+M::::::::M           M::::::::M                             d::::::d  iiii   f::::::::::::::::::f  iiii                                             t:::::t            iiii                                                      
+M:::::::::M         M:::::::::M                             d:::::d          f::::::fffffff:::::f                                                   t:::::t                                                                      
+M::::::::::M       M::::::::::M   ooooooooooo       ddddddddd:::::d iiiiiii  f:::::f       ffffffiiiiiii     cccccccccccccccc  aaaaaaaaaaaaa  ttttttt:::::ttttttt    iiiiiii    ooooooooooo   nnnn  nnnnnnnn        ssssssssss   
+M:::::::::::M     M:::::::::::M oo:::::::::::oo   dd::::::::::::::d i:::::i  f:::::f             i:::::i   cc:::::::::::::::c  a::::::::::::a t:::::::::::::::::t    i:::::i  oo:::::::::::oo n:::nn::::::::nn    ss::::::::::s  
+M:::::::M::::M   M::::M:::::::Mo:::::::::::::::o d::::::::::::::::d  i::::i f:::::::ffffff        i::::i  c:::::::::::::::::c  aaaaaaaaa:::::at:::::::::::::::::t     i::::i o:::::::::::::::on::::::::::::::nn ss:::::::::::::s 
+M::::::M M::::M M::::M M::::::Mo:::::ooooo:::::od:::::::ddddd:::::d  i::::i f::::::::::::f        i::::i c:::::::cccccc:::::c           a::::atttttt:::::::tttttt     i::::i o:::::ooooo:::::onn:::::::::::::::ns::::::ssss:::::s
+M::::::M  M::::M::::M  M::::::Mo::::o     o::::od::::::d    d:::::d  i::::i f::::::::::::f        i::::i c::::::c     ccccccc    aaaaaaa:::::a      t:::::t           i::::i o::::o     o::::o  n:::::nnnn:::::n s:::::s  ssssss 
+M::::::M   M:::::::M   M::::::Mo::::o     o::::od:::::d     d:::::d  i::::i f:::::::ffffff        i::::i c:::::c               aa::::::::::::a      t:::::t           i::::i o::::o     o::::o  n::::n    n::::n   s::::::s      
+M::::::M    M:::::M    M::::::Mo::::o     o::::od:::::d     d:::::d  i::::i  f:::::f              i::::i c:::::c              a::::aaaa::::::a      t:::::t           i::::i o::::o     o::::o  n::::n    n::::n      s::::::s   
+M::::::M     MMMMM     M::::::Mo::::o     o::::od:::::d     d:::::d  i::::i  f:::::f              i::::i c::::::c     ccccccca::::a    a:::::a      t:::::t    tttttt i::::i o::::o     o::::o  n::::n    n::::nssssss   s:::::s 
+M::::::M               M::::::Mo:::::ooooo:::::od::::::ddddd::::::ddi::::::if:::::::f            i::::::ic:::::::cccccc:::::ca::::a    a:::::a      t::::::tttt:::::ti::::::io:::::ooooo:::::o  n::::n    n::::ns:::::ssss::::::s
+M::::::M               M::::::Mo:::::::::::::::o d:::::::::::::::::di::::::if:::::::f            i::::::i c:::::::::::::::::ca:::::aaaa::::::a      tt::::::::::::::ti::::::io:::::::::::::::o  n::::n    n::::ns::::::::::::::s 
+M::::::M               M::::::M oo:::::::::::oo   d:::::::::ddd::::di::::::if:::::::f            i::::::i  cc:::::::::::::::c a::::::::::aa:::a       tt:::::::::::tti::::::i oo:::::::::::oo   n::::n    n::::n s:::::::::::ss  
+MMMMMMMM               MMMMMMMM   ooooooooooo      ddddddddd   dddddiiiiiiiifffffffff            iiiiiiii    cccccccccccccccc  aaaaaaaaaa  aaaa         ttttttttttt  iiiiiiii   ooooooooooo     nnnnnn    nnnnnn  sssssssssss    
+for the Amazing Racer
+
+[1] Directional Light
+Reduced the amount of light to make use of a flashlight.
+[2] Fog
+Made a red fog to give it an apocolyptic atmosphere.
+[3] Skybox
+Sun is bigger, atmosphere is thicker, and the sky tint is orange.
+[4] Player
+Given a "latern" to be able to light up their surroundings. I also made them jump a little lower to make some areas more challenging.
+[5] Finish Zone
+Made 3 finish zones: one green(the main), one blue, and one red (to purposely blend in)
+For the particle effects, I added have a color over lifetime, size over lifetime, a trail, and some noise to it to give it a sort of "random" effect.
+[6] Terrain
+Made the terrain have 3 paths: a linear path(the main), a hidden one, and a hidden one in the hidden one (hidden-ception)
+*/
---
+++ jjamotillo_Final/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "jjamotillo_Final";
+
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
